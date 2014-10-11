30ae81f5d5b5a378f15a9ae8f3951ed9e74cca81
Gayan Virajith <gayanvirajith@gmail.com>
2014-10-11 19:10:35 +0530

    Above the fold css added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,33 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-ruby-sass');
 
-gulp.task('default', function(){
+// Gulp task - uglify-modernizr 
+// Minifiy our modernizr file
+gulp.task('uglify-modernizr', function(){
 	gulp.src('assets/js/modernizr.js')
 	.pipe(uglify())
-	.pipe(gulp.dest('assets/js/min'));
+	.pipe(gulp.dest('assets/js/min'))
+});
+
+
+gulp.task('uglify-css', function(){
+	gulp.src('assets/css/*.css')
+	.pipe(uglify())
+	.pipe(gulp.dest('assets/css/min'))
+});
+
+// Gulp scss
+gulp.task('scss', function (){
+	gulp.src('assets/css/atf.scss')
+	.pipe(sass({
+		style: 'compressed'
+	}))
+	.on('error', function (err) { console.log(err.message); })
+	.pipe(gulp.dest('assets/css/'));
+});
+
+// Default gulp task
+gulp.task('default', function(){
+	
 });
